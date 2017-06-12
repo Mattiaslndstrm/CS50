78c226c1317379845c15ee834475a030c495d009
Mattias Lindström <mattias.lindstrom@gmail.com>
2017-06-12 10:56:08 -0500

    Correct spelling

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -15,7 +15,7 @@ int main(int argc, string argv[])
         string plaintext = get_string();
         // Prints the encoded string from the command line argument stored in 
         // position 1
-        printf("cyphertext: %s\n", vigenere(argv[1], plaintext));
+        printf("ciphertext: %s\n", vigenere(argv[1], plaintext));
     }
 
     else
@@ -34,14 +34,14 @@ string vigenere(string keyword, string plain)
         int diff = tolower(keyword[i % strlen(keyword)]) - 97;
 
         // For uppercase. plain[i] + diff - 13) % 26 equals the position of 
-        // the cypher letter in the alphabet (because 65 % 26 = 13) . Add 65 
+        // the cipher letter in the alphabet (because 65 % 26 = 13) . Add 65 
         // and we get the position of the letter in ASCII.
         if (plain[i] >= 65 && plain[i] <= 90)
         {   
             plain[i] = (plain[i] + diff - 13) % 26 + 65;
         }
         // For lowercase. plain[i] + diff - 19) % 26 equals the position of 
-        // the cypher letter in the alphabet (because 97 % 26 = 19). Add 97 and 
+        // the cipher letter in the alphabet (because 97 % 26 = 19). Add 97 and 
         // we get the position of the letter in ASCII. 
         if (plain[i] >= 97 && plain[i] <= 122)
         {
