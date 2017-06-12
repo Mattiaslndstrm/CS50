af989f558433454807a1c5df11f6450e01bb212f
Mattias Lindström <mattias.lindstrom@gmail.com>
2017-06-12 10:51:03 -0500

    Add working vigenere function, add comments

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -3,21 +3,50 @@
 #include <string.h>
 #include <ctype.h>
 
-string vigenere(string plain);
+string vigenere(string keyword, string plain);
 
 int main(int argc, string argv[]) 
 {   
     if (argc == 2)
+    // Checks if there is one additional command line argument.
     {
         printf("plaintext: ");
+        // Gets the plain text from the user
         string plaintext = get_string();
-        printf("ciphertext: %s\n", vigenere(plaintext));
+        // Prints the encoded string from the command line argument stored in 
+        // position 1
+        printf("cyphertext: %s\n", vigenere(argv[1], plaintext));
     }
 
     else
     {
-        printf("Usage: ./vigenere plaintext");
+        printf("Usage: ./vigenere keyword");
         return 1;
     }
 }
 
+string vigenere(string keyword, string plain)
+{
+    for (int i = 0, n = strlen(plain); i < n; i ++) {
+        // Equals the difference between the current character in keyword  
+        // converted to lowercase and lowercase a. The current position in 
+        // keyword equals i % (length of keyword)
+        int diff = tolower(keyword[i % strlen(keyword)]) - 97;
+
+        // For uppercase. plain[i] + diff - 13) % 26 equals the position of 
+        // the cypher letter in the alphabet (because 65 % 26 = 13) . Add 65 
+        // and we get the position of the letter in ASCII.
+        if (plain[i] >= 65 && plain[i] <= 90)
+        {   
+            plain[i] = (plain[i] + diff - 13) % 26 + 65;
+        }
+        // For lowercase. plain[i] + diff - 19) % 26 equals the position of 
+        // the cypher letter in the alphabet (because 97 % 26 = 19). Add 97 and 
+        // we get the position of the letter in ASCII. 
+        if (plain[i] >= 97 && plain[i] <= 122)
+        {
+            plain[i] = (plain[i] + diff - 19) % 26 + 97;
+        }
+    }
+    return plain;
+}
