3a031523cb267833c5930a75b943e2b7c6c701ca
Mattias Lindström <mattias.lindstrom@gmail.com>
2017-06-13 05:37:11 -0500

    Add check for non alphabetic characters

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -7,36 +7,53 @@ string vigenere(string keyword, string plain);
 
 int main(int argc, string argv[]) 
 {   
-    if (argc == 2)
     // Checks if there is one additional command line argument.
-    {
-        printf("plaintext: ");
-        // Gets the plain text from the user
-        string plaintext = get_string();
-        // Prints the encoded string from the command line argument stored in 
-        // position 1
-        printf("ciphertext: %s\n", vigenere(argv[1], plaintext));
-    }
+    if (argc == 2)
+    
+    {   bool alpha = true;
+        // Iterates through the keyword and changes the boolean alpha to false
+        // if it contains any non-alphabetic characters.
+        for (int j = 0, m = strlen(argv[1]); j < m; j++)
+        {
+            if (!isalpha(argv[1][j]))
+            {
+                alpha = false;
+            }
+        }
 
-    else
-    {
-        printf("Usage: ./vigenere keyword");
-        return 1;
+        if (alpha)
+        {
+            printf("plaintext: ");
+            // Gets the plain text from the user
+            string plaintext = get_string();
+            // Prints the encoded string from the command line argument stored in 
+            // position 1
+            printf("ciphertext: %s\n", vigenere(argv[1], plaintext));
+            // To terminate the program
+            return 0;
+        }
     }
+    // Incorrect use of vigenere. Exit with error.
+    printf("Usage: ./vigenere keyword\n");
+    return 1;
 }
 
 string vigenere(string keyword, string plain)
 {
     int non_alpha = 0;
+    // Iterates through the character of plain 
     for (int i = 0, n = strlen(plain); i < n; i ++) {
-        // Equals the difference between the current character in keyword  
-        // converted to lowercase and lowercase a. The current position in 
-        // keyword equals i % (length of keyword)
-
+        
+        // Adds 1 for every non alphabetic character to the int non_alpha
         if (!isalpha(plain[i]))
         {
             non_alpha += 1;
         }
+
+        // Equals the difference between the current character in the keyword  
+        // converted to lowercase and lowercase a. The current position in 
+        // keyword equals (i minus eventual non-alphabetic characters) % 
+        // (length of keyword)
         int diff = tolower(keyword[(i - non_alpha) % strlen(keyword)]) - 97;
 
         // For uppercase. plain[i] + diff - 13) % 26 equals the position of 
