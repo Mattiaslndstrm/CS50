623eff8197e01a1856bb8cd64d00defd723448f4
Mattias Lindström <mattias.lindstrom@gmail.com>
2017-07-25 13:52:21 -0500

    Refractor makeArray to function, accept arguments

--- pset2/crack/crack.c
+++ pset2/crack/crack.c
@@ -1,20 +1,44 @@
 #define _XOPEN_SOURCE
 #include <unistd.h>
-// #include <cs50.h>
+#include <cs50.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #define AL 52
+#define ARRAY_LENGTH 7314372
+#define STRING_LENGTH 5
 
-void crack(void);
+string crack(string hash);
 char toChar(int num);
+void generateArray(void);
+char arr[ARRAY_LENGTH][STRING_LENGTH] = {0};
 
-// int main(int argc, string argv[])
-
-char arr[7314372][5] = {0};
-int main(void)
+int main(int argc, string argv[])
 {
-    // char arr[7314372][5];
+    if (argc == 2)
+    {
+        generateArray();
+        string hash = argv[1];
+        printf("%s\n", crypt(hash, "abcd"));
+    }
+    else
+    {
+        printf("Usage: ./crack hash\n");
+        return 1;
+    }
+    for (int a = 7314332; a < 7314372; a++)
+    {
+        printf("%i: %s\n", a, arr[a]);
+    }
+}
+
+string crack(string hash)
+{
+
+}
+
+void generateArray(void)
+{
     for (int f = 0; f < AL; f++)
     {
         arr[f][0] = toChar(f);
@@ -42,61 +66,9 @@ int main(void)
             }
         }
     }
-    for (int a = 7314332; a < 7314372; a++)
-    {
-        printf("%i: %s\n", a, arr[a]);
-    }
-
-    // if (argc == 2)
-    // {
-    //     string hash = argv[1];
-    //     printf("%s\n", crypt(hash, "abcd"));
-    // }
-    // else
-    // {
-    //     printf("Usage: ./crack hash\n");
-    //     return 1;
-    // }
 }
-
 
 char toChar(int num)
 {
     return (num < 26) ? (num + 65) : (num + 71);
 }
-
-// void crack(void)
-// {
-//     char arr[500000][5];
-//     for (char f = 97; f < 123; f++)
-//     {
-//         arr[f-97][0] = f;
-//         arr[f-97][1] = '\0';
-//         for (char g = 97; g < 123; g++)
-//         {
-//             arr[g - 97 + 25 + 25*(f-97)][0] = f;
-//             arr[g - 97 + 25 + 25*(f-97)][1] = g;
-//             arr[g - 97 + 25 + 25*(f-97)][2] = '\0';
-//             for (char h = 97; h < 123; h++)
-//             {
-//                 arr[g - 97 + 25 + 25*(f-97) + 25*(g-97)][0] = f;
-//                 arr[g - 97 + 25 + 25*(f-97) + 25*(g-97)][1] = g;
-//                 arr[g - 97 + 25 + 25*(f-97) + 25*(g-97)][2] = h;
-//                 arr[g - 97 + 25 + 25*(f-97) + 25*(g-97)][3] = '\0';
-
-//                 for (char i = 97; i < 123; i++)
-//                 {
-//                     arr[g - 97 + 25 + 25*(f-97) + 25*(g-97) + 25*(h-97)][0] = f;
-//                     arr[g - 97 + 25 + 25*(f-97) + 25*(g-97) + 25*(h-97)][1] = g;
-//                     arr[g - 97 + 25 + 25*(f-97) + 25*(g-97) + 25*(h-97)][2] = h;
-//                     arr[g - 97 + 25 + 25*(f-97) + 25*(g-97) + 25*(h-97)][3] = i;
-//                     arr[g - 97 + 25 + 25*(f-97) + 25*(g-97) + 25*(h-97)][4] = '\0';
-//                 }
-//             }
-//         }
-//     }
-//     for (int a = 0; a > 20000; a++)
-//     {
-//         printf("%s", arr[a]);
-//     }
-// }
