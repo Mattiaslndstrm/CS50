700e77d92a201e32fa16398c34d380a7a1b9af78
Mattias Lindström <mattias.lindstrom@gmail.com>
2019-06-17 09:48:16 +0200

    Open file and look for jpeg-header

--- pset3/recover/recover.c
+++ pset3/recover/recover.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -9,4 +10,15 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    FILE *image = fopen(argv[1], "r");
+    int i, j = 0, k = 0;
+    while((i = getc(image)) != EOF)
+    {
+        if (j == 0xff && i == 0xd8)
+            k++;
+        j = i;
+        // printf("%x", i);
+
+    }
+    printf("Number of jpgegs: %d\n", k);
 }
